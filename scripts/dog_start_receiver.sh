9b25fdef6f79f800ca8047ce90a08a38b765dee6
Jayce Spurgiasz <jayceeasparagus@gmail.com>
2026-07-07 13:48:30 -0700

    revert service

--- scripts/dog_start_receiver.sh
+++ scripts/dog_start_receiver.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
-set -e
+set -eu
 
-APP_DIR="${APP_DIR:-/home/unitree/Voice-Interface-V2}"
+APP_DIR="${APP_DIR:-/home/unitree/Voice-Interface-V2/Voice-Interface-V2}"
 cd "$APP_DIR"
-
-if [ -f /opt/ros/foxy/setup.sh ]; then
-  . /opt/ros/foxy/setup.sh
-fi
-
-if [ -f /home/unitree/unitree_ros2/setup.sh ]; then
-  . /home/unitree/unitree_ros2/setup.sh
-fi
-
-if [ -f /home/unitree/unitree_ros2/setup_local.sh ]; then
-  . /home/unitree/unitree_ros2/setup_local.sh
-fi
 
 export PYTHONPATH="/home/unitree/unitree_sdk2_python:${PYTHONPATH:-}"
 export PYTHONPATH="/home/unitree/.local/lib/python3.8/site-packages:${PYTHONPATH:-}"
+export LD_LIBRARY_PATH="/home/unitree/unitree_ros2/cyclonedds_ws/install/cyclonedds/lib:${LD_LIBRARY_PATH:-}"
 
 echo "Starting dog command receiver from $APP_DIR"
 exec python3 -u "$APP_DIR/dog/receiver.py" --host "${DOG_WIRED_IP:-10.42.0.1}" --port "${DOG_COMMAND_PORT:-5005}"
